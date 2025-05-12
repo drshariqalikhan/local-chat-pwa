6afe2e5bfc2691ed92b6e92513b961e78ae6fa4e
drshariqalikhan <31785258+drshariqalikhan@users.noreply.github.com>
2025-05-12 08:32:55 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,53 @@
+const CACHE_NAME = 'local-chat-v1';
+const URLS_TO_CACHE = [
+  '.', // Alias for index.html
+  'index.html',
+  'style.css',
+  'script.js',
+  'manifest.json',
+  'js/qrcode.min.js',
+  'js/jsQR.js',
+  'icons/icon-192x192.png',
+  'icons/icon-512x512.png'
+];
 
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(URLS_TO_CACHE);
+      })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request); // Or implement more sophisticated caching
+      }
+    )
+  );
+});
+
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+  return self.clients.claim();
+});
